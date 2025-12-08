b9206c207753d8853946f61045e3d4699d14649b
ShadowXX-08 <abduqodirovdavlatbek08@gmail.com>
2025-12-08 20:32:39 +0500

    hozircha frontend yakunlandi

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdminDashboard from "./pages/AdminDashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import AdminRoute from "./routes/AdminRoute";
+import ProductEditScreen from "./pages/ProductEditScreen";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900">
+      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+      <Navbar />
+      <main className="grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route element={<ProtectedRoute />}>
+            <Route path="/cart" element={<Cart />} />
+          </Route>
+
+          <Route element={<AdminRoute />}>
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
+          </Route>
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
