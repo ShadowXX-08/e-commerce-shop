136929c1546acc1045c92c526ece3b150ec80f2e
ShadowXX-08 <abduqodirovdavlatbek08@gmail.com>
2025-12-17 18:39:38 +0500

    Railway config fayl qo'shildi

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://e-commerce-shop-backend.onrender.com/api";
+const API_URL = import.meta.env.VITE_API_URL || "https://e-commerce-shop-production-8935.up.railway.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
