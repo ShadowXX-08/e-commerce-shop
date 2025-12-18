0cd03f1313c6aa9ad4b6dd0a09cfc956def82fa2
ShadowXX-08 <abduqodirovdavlatbek08@gmail.com>
2025-12-18 13:29:56 +0500

    server.js va vercel.json axshilandi

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const helmet = require("helmet");
-const compression = require("compression"); // <-- QO'SHILDI
+const compression = require("compression");
 const connectDB = require("./config/db");
 const path = require("path");
 
@@ -13,7 +13,17 @@ connectDB();
 const app = express();
 
 app.use(compression());
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    "https://e-commerce-shop-front-end.vercel.app",
+    "http://localhost:5173", 
+    "http://localhost:3000"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true 
+}));
+
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
@@ -22,15 +32,22 @@ app.use(
 );
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/upload", require("./routes/uploadRoutes"));
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// 404 Handler
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not Found" });
 });
 
+// Error Handler
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
