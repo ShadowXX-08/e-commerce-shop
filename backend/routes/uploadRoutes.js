06c8d42c5137016eac56a746f87597085cd8809c
ShadowXX-08 <abduqodirovdavlatbek08@gmail.com>
2025-12-20 04:11:45 +0500

    Routesdagi uploadRoutesdagi kichik o'zgarish

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middleware/upload'); 
+const upload = require('../middleware/upload');
 const { protect, admin } = require('../middleware/authMiddleware');
 
+router.post('/', protect, admin, (req, res) => {
+  upload.single('image')(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ message: err.message });
+    }
 
-router.post('/', protect, admin, upload.single('image'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send({ message: 'Rasm yuklanmadi' });
-  }
+    if (!req.file) {
+      return res.status(400).send({ message: 'Rasm tanlanmagan (image field is missing)' });
+    }
 
-  res.send({
-    message: 'Image uploaded successfully',
-    image: req.file.path
+    res.send({
+      message: 'Image uploaded successfully',
+      image: req.file.path
+    });
   });
 });
 
